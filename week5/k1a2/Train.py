6a7bd4b31145b8ffe089ac18a73029f1d75e5c3d
K1A2 <jckim0307@gmail.com>
2021-11-23 23:55:23 +0900

    [FIX] gpu memory growth

--- week5/k1a2/Train.py
+++ week5/k1a2/Train.py
@@ -7,6 +7,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3072)])
+        tf.config.experimental.set_memory_growth(gpus[0], True)
     except RuntimeError as e:
         print(e)
 
